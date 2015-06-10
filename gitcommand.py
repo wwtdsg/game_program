7f75654b6ca1b5ef7daf2bbaf152411cc92c797e
wwtdsg <wwtdsg@gmail.com>
2015-06-10 18:40:55 +0800

    test

--- gitcommand.py
+++ gitcommand.py
@@ -4,4 +4,4 @@ import os
 string = raw_input("输入提交备注: ")
 print os.popen('git add .').read()
 print os.popen('git ci -m "%s"' % string).read()
-print os.popen('git push oring master').read()
+print os.popen('git push origin master').read()
