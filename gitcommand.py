b1fab7539813fcf710d017fff4a150ac99cf12d2
wwtdsg <wwtdsg@gmail.com>
2015-06-10 18:52:01 +0800

    test

--- gitcommand.py
+++ gitcommand.py
@@ -1,8 +1,13 @@
 #! -*- encoding:utf-8 -*-
 import os
+import re
+import sys
 
-print '当前工作区变化：'
-print os.popen('git st').read()
+print '当前工作区情况：'
+st = os.popen('git st').read()
+if re.search('fatal', st):
+    print st
+    sys.exit()
 string = raw_input("输入提交备注: ")
 print os.popen('git add .').read()
 print os.popen('git ci -m "%s"' % string).read()
