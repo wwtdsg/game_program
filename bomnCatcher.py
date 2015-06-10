b631f74cb5bf5cc14e7ec3cc3135c263323bce7c
wwtdsg <wwtdsg@gmail.com>
2015-06-10 18:39:50 +0800

    test

--- bomnCatcher.py
+++ bomnCatcher.py
@@ -4,39 +4,104 @@ import sys
 import random
 
 
+pygame.init()
+screen = pygame.display.set_mode((600, 500))
+font1 = pygame.font.Font(None, 24)
+pygame.mouse.set_visible(False)
+lives = 3
+score = 0
+game_over = True
+
+
 def print_text(font, x, y, text, color=(255, 255, 255)):
     imgText = font.render(text, True, color)
     screen.blit(imgText, (x, y))
 
-pygame.init()
-screen = pygame.display.set_mode((600, 500))
-pygame.display.set_caption("Bomb Catching Game")
-font1 = pygame.font.Font(None, 24)
-pygame.mouse.set_visible(False)
-white = 255, 255, 255
-yellow = 230, 230, 50
-black = 0, 0, 0
 
-lives = 3
-score = 0
-game_over = True
-mouse_x = mouse_y = 0
-pos_x = 300
-pos_y = 460
-bomb_x = random.randint(0, 500)
-bomb_y = -50
-vel_y = 0.7
-r = 200
-g = 50
-b = 50
+def collision(bomb, board):
+    global flag
+    if bomb.y > 500:
+        bomb.x = random.randint(15, 485)
+        bomb.y = -50
+        global lives
+        lives -= 1
+        bomb.vel = 0.7
+        bomb.color_change()
+        flag = True
+        if lives == 0:
+            global game_over
+            game_over = True
+    elif bomb.y > board.y:
+        if bomb.x > board.x and bomb.x < board.x + 120:
+            global score
+            score += 10
+            bomb.accelerate(0.2)
+            bomb.x = random.randint(15, 485)
+            bomb.y = -50
+            board.color_change()
+            bomb.color_change()
+            flag = True
 
+
+class Bomb():
+    def __init__(self):
+        self.x = random.randint(15, 485)
+        self.y = -50
+        self.vel = 0.7
+        self.color = 230, 230, 50
+
+    def accelerate(self, dv):
+        self.vel += dv
+
+    def move(self):
+        self.y += self.vel
+
+    def color_change(self):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.color = r, g, b
+
+    def creat(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x - 4, int(self.y - 4)), 30, 0)
+
+
+class Board():
+    def __init__(self):
+        self.x = 300
+        self.y = 460
+        self.r = 200
+        self.g = 40
+        self.b = 40
+
+    def color_change(self):
+        self.r = random.randint(0, 255)
+        self.g = random.randint(0, 255)
+        self.b = random.randint(0, 255)
+
+    def move(self, dy):
+        self.y += dy
+
+    def creat(self, screen):
+        pygame.draw.rect(screen, (self.r, self.g, self.b), (self.x, self.y, 120, 40), 0)
+
+
+board = Board()
+bomb = Bomb()
+mouse_x = 0
+flag = True
 while True:
+    if flag:
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        flag = False
+    screen.fill((r, g, b))
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit()
         elif event.type == MOUSEMOTION:
             mouse_x, mouse_y = event.pos
-            move_x, move_y = event.rel
         elif event.type == MOUSEBUTTONUP:
             if game_over:
                 game_over = False
@@ -47,43 +112,14 @@ while True:
     if keys[K_ESCAPE]:
         sys.exit()
 
-    screen.fill((0, 0, 100))
-
+    board.x = mouse_x
     if game_over:
-        print_text(font1, 250, 200, "<click to play>")
+        print_text(font1, 250, 200, "<CLICK TO PLAY>")
     else:
-        bomb_y += vel_y
-        if bomb_y > 500:
-            bomb_x = random.randint(0, 500)
-            bomb_y = -50
-            lives -= 1
-            if lives == 0:
-                vel_y = 0.7
-                game_over = True
-
-        elif bomb_y > pos_y:
-            if bomb_x > pos_x and bomb_x < pos_x + 120:
-                score += 10
-                vel_y += 0.2
-                bomb_x = random.randint(0, 500)
-                bomb_y = -50
-                r = random.randint(0, 255)
-                g = random.randint(0, 255)
-                b = random.randint(0, 255)
-
-        pygame.draw.circle(screen, black, (bomb_x - 4, int(bomb_y - 4)), 30, 0)
-        pygame.draw.circle(screen, yellow, (bomb_x, int(bomb_y)), 30, 0)
-
-        pos_x = mouse_x
-        if pos_x < 0:
-            pos_x = 0
-        elif pos_x > 500:
-            pos_x = 500
-
-        pygame.draw.rect(screen, black, (pos_x - 4, pos_y - 4, 120, 40), 0)
-        pygame.draw.rect(screen, (r, g, b), (pos_x, pos_y, 120, 40), 0)
-
+        bomb.move()
+        collision(bomb, board)
+        bomb.creat(screen)
+    board.creat(screen)
     print_text(font1, 0, 0, "LIVES: " + str(lives))
-    print_text(font1, 300, 0, "SCORES: " + str(score))
-
+    print_text(font1, 300, 0, "SCORE: " + str(score))
     pygame.display.update()
---
+++ gitcommand.py
@@ -0,0 +1,7 @@
+#! -*- encoding:utf-8 -*-
+import os
+
+string = raw_input("输入提交备注: ")
+print os.popen('git add .').read()
+print os.popen('git ci -m "%s"' % string).read()
+print os.popen('git push oring master').read()
