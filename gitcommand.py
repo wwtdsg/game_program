d7119e38c4983d97815c2c82f372f8e11e9b6101
wwtdsg <wwtdsg@gmail.com>
2015-06-10 18:55:47 +0800

    gitcommand change

--- gitcommand.py
+++ gitcommand.py
@@ -5,10 +5,11 @@ import sys
 
 print '当前工作区情况：'
 st = os.popen('git st').read()
+print st
 if re.search('fatal', st):
-    print st
     sys.exit()
-string = raw_input("输入提交备注: ")
-print os.popen('git add .').read()
-print os.popen('git ci -m "%s"' % string).read()
-print os.popen('git push origin master').read()
+else:
+    string = raw_input("输入提交备注: ")
+    print os.popen('git add .').read()
+    print os.popen('git ci -m "%s"' % string).read()
+    print os.popen('git push origin master').read()
