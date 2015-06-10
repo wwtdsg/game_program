a45df0a0021cca2d48f7ed0b561499ee7d5903b5
wwtdsg <wwtdsg@gmail.com>
2015-06-10 18:44:01 +0800

    aa

--- gitcommand.py
+++ gitcommand.py
@@ -1,6 +1,8 @@
 #! -*- encoding:utf-8 -*-
 import os
 
+print '当前工作区变化：'
+print os.popen('git st').read()
 string = raw_input("输入提交备注: ")
 print os.popen('git add .').read()
 print os.popen('git ci -m "%s"' % string).read()
