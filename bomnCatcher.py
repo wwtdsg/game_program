b6a176f8e2d0fdf53b72dbce8dea5761c30349f0
wwtdsg <wwtdsg@gmail.com>
2015-06-13 21:59:50 +0800

    game change

--- bomnCatcher.py
+++ bomnCatcher.py
@@ -1,3 +1,4 @@
+#!-*- encoding:utf-8 -*-
 import pygame
 from pygame.locals import *
 import sys
@@ -7,10 +8,23 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((600, 500))
 font1 = pygame.font.Font(None, 24)
+font2 = pygame.font.Font(None, 34)
+font3 = pygame.font.Font(None, 44)
 pygame.mouse.set_visible(False)
 lives = 3
 score = 0
 game_over = True
+
+
+def compare(a, b):
+    e1 = int(a.strip())
+    e2 = int(b.strip())
+    if e1 < e2:
+        return -1
+    elif e1 == e2:
+        return 0
+    else:
+        return 1
 
 
 def print_text(font, x, y, text, color=(255, 255, 255)):
@@ -25,17 +39,16 @@ def collision(bomb, board):
         bomb.y = -50
         global lives
         lives -= 1
-        bomb.vel = 0.7
-        bomb.color_change()
-        flag = True
+        bomb.vel = 1.7
         if lives == 0:
             global game_over
             game_over = True
     elif bomb.y > board.y:
-        if bomb.x > board.x and bomb.x < board.x + 120:
+        if bomb.x > board.x - 15 and bomb.x < board.x + 120:
             global score
             score += 10
             bomb.accelerate(0.2)
+            flag = True
             bomb.x = random.randint(15, 485)
             bomb.y = -50
             board.color_change()
@@ -47,8 +60,8 @@ class Bomb():
     def __init__(self):
         self.x = random.randint(15, 485)
         self.y = -50
-        self.vel = 0.7
-        self.color = 230, 230, 50
+        self.vel = 1.7
+        self.color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
     def accelerate(self, dv):
         self.vel += dv
@@ -70,26 +83,27 @@ class Board():
     def __init__(self):
         self.x = 300
         self.y = 460
-        self.r = 200
-        self.g = 40
-        self.b = 40
+        self.color = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
     def color_change(self):
-        self.r = random.randint(0, 255)
-        self.g = random.randint(0, 255)
-        self.b = random.randint(0, 255)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.color = r, g, b
 
     def move(self, dy):
         self.y += dy
 
     def creat(self, screen):
-        pygame.draw.rect(screen, (self.r, self.g, self.b), (self.x, self.y, 120, 40), 0)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, 120, 40), 0)
 
 
 board = Board()
 bomb = Bomb()
 mouse_x = 0
 flag = True
+game_start = True
+record_posible = True
 while True:
     if flag:
         r = random.randint(0, 255)
@@ -103,6 +117,8 @@ while True:
         elif event.type == MOUSEMOTION:
             mouse_x, mouse_y = event.pos
         elif event.type == MOUSEBUTTONUP:
+            game_start = False
+            record_posible = True
             if game_over:
                 game_over = False
                 lives = 3
@@ -111,10 +127,39 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[K_ESCAPE]:
         sys.exit()
-
     board.x = mouse_x
-    if game_over:
-        print_text(font1, 250, 200, "<CLICK TO PLAY>")
+
+    if game_start:
+        print_text(font1, 250, 250, "<CLICK TO PLAY>")
+    elif game_over:
+        # 文件处理，记录保存
+        if record_posible:
+            record_posible = False
+            ran = 0
+            try:
+                f = open(r'bomb_record.txt')
+            except:
+                f = open(r'bomb_record.txt', 'w')
+                f.close()
+                f = open(r'bomb_record.txt')
+            records = f.readlines()
+            new_records = records[:]
+            f.close()
+            new_records.append(str(score) + '\n')
+            new_records.sort(cmp=compare, reverse=True)
+            ran = new_records.index(str(score) + '\n')
+            f = open(r'bomb_record.txt', 'w')
+            for line in new_records:
+                f.write(line)
+            f.close()
+        print_text(font1, 40, 250, "YOU SCORED %d POINTS, RANGE %d" % (score, ran + 1))
+        if ran == 0:
+            print_text(font3, 40, 100, "Wanderful!")
+            print_text(font3, 40, 150, "You just made a new record!")
+        else:
+            print_text(font2, 40, 200, "GAME OVER!")
+        print_text(font1, 40, 300, "<CLICK TO TRY AGAIN>")
+
     else:
         bomb.move()
         collision(bomb, board)
---
+++ test.py
@@ -0,0 +1,4 @@
+f = open('bomb_record.txt')
+m = f.readlines()
+print m
+f.close()
